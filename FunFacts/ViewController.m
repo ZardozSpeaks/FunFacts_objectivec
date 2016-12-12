995d2a5e1c48de87aa6a80a03b0e68608ddf85ce
David Remington <david.ian.remington@gmail.com>
2016-12-12 13:45:01 -0800

    refactor code for claitity and reusablity

--- FunFacts/ViewController.m
+++ FunFacts/ViewController.m
@@ -18,7 +18,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.factBook = [[FactBook alloc] init];
-    self.funFactLabel.text = [self.factBook.facts objectAtIndex:0];
+    self.funFactLabel.text = [self.factBook randomFact];
 
 }
 
@@ -28,7 +28,7 @@
 }
 
 - (IBAction)showFunFact {
-        self.funFactLabel.text = [self.factBook.facts objectAtIndex:1];
+        self.funFactLabel.text = [self.factBook randomFact];
 }
 
 @end
