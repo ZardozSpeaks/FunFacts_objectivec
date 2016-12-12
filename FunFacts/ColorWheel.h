909f454ac1e8aeef6dba466437fe1417967b077e
David Remington <david.ian.remington@gmail.com>
2016-12-12 14:26:39 -0800

    add constraints and stylistic improvements to view

---
+++ FunFacts/ColorWheel.h
@@ -0,0 +1,13 @@
+//
+//  ColorWheel.h
+//  FunFacts
+//
+//  Created by David Remington on 12/12/16.
+//  Copyright © 2016 David Remington. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ColorWheel : NSObject
+
+@end
---
+++ FunFacts/ColorWheel.m
@@ -0,0 +1,13 @@
+//
+//  ColorWheel.m
+//  FunFacts
+//
+//  Created by David Remington on 12/12/16.
+//  Copyright © 2016 David Remington. All rights reserved.
+//
+
+#import "ColorWheel.h"
+
+@implementation ColorWheel
+
+@end
