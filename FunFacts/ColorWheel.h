7725ed300709d8173640b3298d8ba63dfee0467a
David Remington <david.ian.remington@gmail.com>
2016-12-12 16:43:38 -0800

    improve color and design of UI

--- FunFacts/ColorWheel.h
+++ FunFacts/ColorWheel.h
@@ -7,7 +7,12 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface ColorWheel : NSObject
+
+@property (strong, nonatomic) NSArray *colors;
+
+- (UIColor *)randomColor;
 
 @end
--- FunFacts/ViewController.m
+++ FunFacts/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "FactBook.h"
+#import "ColorWheel.h"
 
 @interface ViewController ()
 
@@ -18,6 +19,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.factBook = [[FactBook alloc] init];
+    self.colorWheel = [[ColorWheel alloc]init];
+    
+    UIColor *randomColor = [self.colorWheel randomColor];
+    self.view.backgroundColor = randomColor;
+    self.funFactButton.tintColor = randomColor;
     self.funFactLabel.text = [self.factBook randomFact];
 
 }
@@ -28,7 +34,10 @@
 }
 
 - (IBAction)showFunFact {
-        self.funFactLabel.text = [self.factBook randomFact];
+    UIColor *randomColor = [self.colorWheel randomColor];
+    self.view.backgroundColor = randomColor;
+    self.funFactButton.tintColor = randomColor;
+    self.funFactLabel.text = [self.factBook randomFact];
 }
 
 @end
